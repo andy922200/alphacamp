1a25c6bef507b3e700e91f74d5fbe89869846b56
andysml <andy0922ucsgit@gmail.com>
2019-06-18 17:24:11 +0200

    Add part of edit function

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,11 +3,19 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../models/restaurant.js')
 
-// display a detail
+// display a restaurant
 router.get('/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
     return res.render('detail', { css: ['detail.css'], restaurant: restaurant })
+  })
+})
+
+// modify specific layout
+router.get('/:id/edit', (req, res) => {
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('edit', { css: ['edit.css'], restaurant: restaurant })
   })
 })
 
