60cbe5ecb3d9ff687034e8aa1e57513cac50ceaa
andysml <andy0922ucsgit@gmail.com>
2019-06-18 05:03:45 +0100

    Update Restaurant List

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -9,9 +9,9 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('db connected!')
-  for (let i = 0; i < 10; i++) {
+  for (let i = 1; i <= 10; i++) {
     Restaurant.create({
-      id: i, name: 'name' + i, name_en: 'name_en' + i, category: 'category' + i, image: 'https://picsum.photos/id/' + (i + 1) + '/200/200', location: 'location' + i, phone: 'phone' + i, google_map: 'google_map' + i, rating: 3, description: 'description' + i
+      id: i, name: 'name' + i, name_en: 'name_en' + i, category: 'category' + i, image: 'https://picsum.photos/id/' + i + '/200/200', location: 'location' + i, phone: 'phone' + i, google_map: 'google_map' + i, rating: i, description: 'description' + i
     })
     console.log('done')
   }
