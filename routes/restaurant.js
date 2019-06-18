c71284f14671b3d687ca267ec3437ede77d45521
andysml <andy0922ucsgit@gmail.com>
2019-06-19 01:44:30 +0200

    add create function and update layout

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,6 +3,31 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../models/restaurant.js')
 
+// specific add page
+router.get('/new', (req, res) => {
+  return res.render('new', { css: ['edit.css'] })
+})
+
+// create a new restaurant
+router.post('/', (req, res) => {
+  const restaurant = Restaurant({
+    name: req.body.inputZhName,
+    name_en: req.body.inputEnName,
+    category: req.body.inputCategory,
+    image: req.body.inputImageURL,
+    location: req.body.inputAddress,
+    phone: req.body.inputPhone,
+    google_map: req.body.inputGoogleMapURL,
+    rating: req.body.inputRating,
+    description: req.body.inputDescription
+  })
+  restaurant.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+  })
+})
+
+
 // display a restaurant
 router.get('/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
@@ -11,7 +36,7 @@ router.get('/:id', (req, res) => {
   })
 })
 
-// specific modification layout
+// specific modification page
 router.get('/:id/edit', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
