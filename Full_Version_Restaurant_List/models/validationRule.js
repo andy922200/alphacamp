cf38227a80884655b76443e109834710bdfec4b3
andysml <andy922200@gmail.com>
2019-06-26 13:04:01 +0100

    seperate validationRule from restaurant.js

---
+++ Full_Version_Restaurant_List/models/validationRule.js
@@ -0,0 +1,53 @@
+// initialize router
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../models/restaurant.js')
+
+// initialize express-validator
+const { check, validationResult } = require('express-validator')
+
+// validator array
+const phonePattern = new RegExp("0[0-9]{1,2}\-[0-9]{7,8}")
+const imageURLPattern = new RegExp("(https?:\/\/.*?\.(?:png|jpe?g|gif)(.*))(\w|$)", "i")
+const googleMapPattern = new RegExp("https://goo.gl/maps/[A-Za-z0-9]*", "i")
+const categories = ["中東料理", "日式料理", "義式料理", "美式料理", "酒吧", "咖啡", "中式料理", "韓式料理"]
+let conditions =
+  [
+    check('inputPhone')
+      .exists()
+      .isLength({ min: 10 })
+      .custom((value) => {
+        return phonePattern.test(value)
+      })
+      .withMessage("電話格式要為區碼-號碼，如：02-23939889"),
+    check('inputRating')
+      .exists()
+      .isNumeric()
+      .custom((value) => {
+        return ((value >= 0) && (value <= 5))
+      })
+      .withMessage('請檢查數字範圍'),
+    check('inputImageURL')
+      .exists()
+      .custom((value) => {
+        return imageURLPattern.test(value)
+      })
+      .withMessage('請檢查圖片網址'),
+    check('inputGoogleMapURL')
+      .custom((value) => {
+        if (!value) {
+          return true
+        } else {
+          return googleMapPattern.test(value)
+        }
+      })
+      .withMessage('請檢查輸入的 Google Map 地址格式'),
+    check('inputCategory')
+      .exists()
+      .custom((value) => {
+        return (categories.indexOf(value) > -1)
+      })
+      .withMessage('請檢查輸入的類別是否正確')
+  ]
+
+module.exports = conditions
--- Full_Version_Restaurant_List/routes/restaurant.js
+++ Full_Version_Restaurant_List/routes/restaurant.js
@@ -4,47 +4,7 @@ const router = express.Router()
 const Restaurant = require('../models/restaurant.js')
 // initialize express-validator
 const { check, validationResult } = require('express-validator')
-
-// validator array
-const phonePattern = new RegExp("0[0-9]{1,2}\-[0-9]{7,8}")
-const imageURLPattern = new RegExp("(https?:\/\/.*?\.(?:png|jpe?g|gif)(.*))(\w|$)", "i")
-const googleMapPattern = new RegExp("https://goo.gl/maps/[A-Za-z0-9]*", "i")
-const categories = ["中東料理", "日式料理", "義式料理", "美式料理", "酒吧", "咖啡", "中式料理", "韓式料理"]
-let conditions =
-  [
-    check('inputPhone')
-      .exists()
-      .isLength({ min: 10 })
-      .custom((value) => {
-        return phonePattern.test(value)
-      })
-      .withMessage("電話格式要為區碼-號碼，如：02-23939889"),
-    check('inputRating')
-      .exists()
-      .isNumeric()
-      .custom((value) => {
-        return ((value >= 0) && (value <= 5))
-      })
-      .withMessage('請檢查數字範圍'),
-    check('inputImageURL')
-      .exists()
-      .custom((value) => {
-        return imageURLPattern.test(value)
-      })
-      .withMessage('請檢查圖片網址'),
-    check('inputGoogleMapURL')
-      .exists()
-      .custom((value) => {
-        return googleMapPattern.test(value)
-      })
-      .withMessage('請檢查輸入的 Google Map 地址格式'),
-    check('inputCategory')
-      .exists()
-      .custom((value) => {
-        return (categories.indexOf(value) > -1)
-      })
-      .withMessage('請檢查輸入的類別是否正確')
-  ]
+const conditions = require('../models/validationRule')
 
 // specific add page
 router.get('/new', (req, res) => {
@@ -105,7 +65,7 @@ router.get('/filter', (req, res) => {
       break
     case 'category':
       let categories = {
-        $or: [{ "category": "中東料理" }, { "category": "日式料理" }, { "category": "義式料理" }, { "category": "美式料理" }, { "category": "酒吧" }, { "category": "咖啡廳" }, { "category": "中式料理" }, { "category": "韓式料理" }]
+        $or: [{ "category": "中東料理" }, { "category": "日式料理" }, { "category": "義式料理" }, { "category": "美式料理" }, { "category": "酒吧" }, { "category": "咖啡" }, { "category": "中式料理" }, { "category": "韓式料理" }]
       }
       Restaurant.find(categories, (err, restaurants) => {
         if (err) return console.error(err)
