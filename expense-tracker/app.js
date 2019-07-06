a8dfe8aa10a93d842beef15ab737d33dc38a85ad
andysml <andy922200@gmail.com>
2019-07-06 14:45:25 +0100

    'Modify': path in app.js

--- expense-tracker/app.js
+++ expense-tracker/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const handlebarsHelper = require('../expense-tracker/public/javascripts/handlebarsHelper')
+const handlebarsHelper = require('./public/javascripts/handlebarshelper')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
