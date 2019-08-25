f4ade745ce7e36c0e6cae33eb87f44e6a11b6e86
andysml <andy922200@gmail.com>
2019-08-25 21:32:03 +0100

    Front End initialize

--- restaurant_forum/app.js
+++ restaurant_forum/app.js
@@ -4,9 +4,14 @@ const app = express()
 const port = 3000
 
 // initialize template engine
-app.engine('handlebars', expressHandlebars())
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// initialize routes settings
+require('./routes')(app)
+
+
+// initialize app listener
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`)
 })
