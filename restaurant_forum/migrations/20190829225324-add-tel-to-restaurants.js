8d4b0ab86ce0e2a58f2f4ca35866294706e8569a
andysml <andy922200@gmail.com>
2019-08-29 23:58:04 +0100

    Add tel to restaurants

---
+++ restaurant_forum/migrations/20190829225324-add-tel-to-restaurants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Restaurants', 'tel', {
+      type: Sequelize.STRING
+    })
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Restaurants', 'tel', {
+      type: Sequelize.STRING
+    })
+
+  }
+};
--- restaurant_forum/models/restaurant.js
+++ restaurant_forum/models/restaurant.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Restaurant = sequelize.define('Restaurant', {
     name: DataTypes.STRING,
+    tel: DataTypes.STRING,
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT,
